48f4b98e3ac04aacb6eec617488b8c96ba52e281
Seiya Miyazaki <68964442+samiyazaki@users.noreply.github.com>
2023-06-12 08:35:32 -0400

    Corrected calculations for board feet

--- script.js
+++ script.js
@@ -5,12 +5,16 @@ document.getElementById('calculate-btn').addEventListener('click', function() {
     const pieces = document.getElementById('pieces').value;
 
     if(length && width && thickness && pieces) {
-        length = eval(length);
+        length = eval(length) ;
         width = eval(width);
         thickness = eval(thickness);
 
+        console.log(`Parsed values - Length: ${length}, Width: ${width}, Thickness: ${thickness}, Pieces: ${pieces}`);  // Log the parsed values
+
         const linearFeet = length * pieces;
-        const boardFeet = (width * thickness * length * pieces) / 144;
+        const boardFeet = (width * thickness * (length * 12) * pieces) / 144; // correct calculation for board feet
+
+        console.log(`Calculated board feet: ${boardFeet}`);  // Log the calculated board feet
 
         document.getElementById('linear-feet').innerText = `Linear Feet: ${linearFeet}`;
         document.getElementById('board-feet').innerText = `Board Feet: ${boardFeet.toFixed(2)}`;
