c6f357188e4895fadb15d593d82f1736433ff042
Seiya Miyazaki <68964442+samiyazaki@users.noreply.github.com>
2023-06-12 08:45:19 -0400

    Adding in local storage and save function

--- script.js
+++ script.js
@@ -5,21 +5,43 @@ document.getElementById('calculate-btn').addEventListener('click', function() {
     const pieces = document.getElementById('pieces').value;
 
     if(length && width && thickness && pieces) {
-        length = eval(length) ;
+        length = eval(length) * 12; // convert length from feet to inches
         width = eval(width);
         thickness = eval(thickness);
 
-        console.log(`Parsed values - Length: ${length}, Width: ${width}, Thickness: ${thickness}, Pieces: ${pieces}`);  // Log the parsed values
+        const linearFeet = length * pieces / 12; // convert length back to feet for linear feet calculation
+        const boardFeet = (width * thickness * length * pieces) / 144; // correct calculation for board feet
 
-        const linearFeet = length * pieces;
-        const boardFeet = (width * thickness * (length * 12) * pieces) / 144; // correct calculation for board feet
-
-        console.log(`Calculated board feet: ${boardFeet}`);  // Log the calculated board feet
-
-        document.getElementById('linear-feet').innerText = `Linear Feet: ${linearFeet}`;
+        document.getElementById('linear-feet').innerText = `Linear Feet: ${linearFeet.toFixed(2)}`;
         document.getElementById('board-feet').innerText = `Board Feet: ${boardFeet.toFixed(2)}`;
     } else {
         alert('Please enter valid values for length, width, thickness, and number of pieces!');
     }
 });
 
+document.getElementById('save-btn').addEventListener('click', function() {
+    const label = document.getElementById('save-label').value;
+    const linearFeet = document.getElementById('linear-feet').innerText;
+    const boardFeet = document.getElementById('board-feet').innerText;
+
+    if(label && linearFeet && boardFeet) {
+        const calculation = {
+            label: label,
+            linearFeet: linearFeet,
+            boardFeet: boardFeet
+        };
+
+        // Get existing calculations from local storage
+        let calculations = JSON.parse(localStorage.getItem('calculations')) || [];
+
+        // Add new calculation to the list
+        calculations.push(calculation);
+
+        // Save the updated list back to local storage
+        localStorage.setItem('calculations', JSON.stringify(calculations));
+
+        alert('Calculation saved!');
+    } else {
+        alert('Please make a calculation before saving!');
+    }
+});
